2e2a2735a209649ca2d3a9da95bc1452ba98d917
Vinay Kumar Sain <your-email@example.com>
2025-07-23 17:18:01 +0530

    well

--- backend/server.js
+++ backend/server.js
@@ -461,3 +461,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
